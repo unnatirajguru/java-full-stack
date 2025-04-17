e039f0ff47520a16f1c8961d76f1fb3d7695bed1
Unnati Sanjay Rajguru <145033110+unnatirajguru@users.noreply.github.com>
2025-04-17 21:57:32 +0530

    Add files via upload

---
+++ Qspider pune/java programming/04-02/while1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class while1
+{
+ public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in);
+ int a=0;
+System.out.println("     Welcome to the Numbers    ");
+while (a<=100)
+ {
+  System.out.print("Number:"+a+" ");
+  a++;
+}
+}
+}
